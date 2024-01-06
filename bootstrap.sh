17d31d2c75f033e3606f2075d46ef0189247c76e
Riley Snyder <rileysnyder@protonmail.ch>
2024-01-05 23:09:05 -0600

    jq venv go ohmyzsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,9 @@
 
 sudo apt-get update
 
-sudo apt-get install -y zsh
+sudo apt-get install -y zsh jq python3.10-venv
+
+curl -LO https://go.dev/dl/go1.21.5.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
